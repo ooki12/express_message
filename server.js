18a0e31693cc93384b5e12b8aab1f4e2e8cb00ec
ooki12 <190245hc@yse-c.net>
2020-12-07 12:07:51 +0900

    first add

--- server.js
+++ server.js
@@ -4,6 +4,30 @@ const express = require('express');
 //サーバ作成
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// ミドルウェア関数
+// 全てのリクエスト
+app.use((req, res, next) => {
+    console.log(`middleware: all. url: ${req.url}`);
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
+});
+
+app.post('/', (req, res) => {
+    let result ={
+        message:req.body.message,
+        datetime: new Date(),
+    };
+    res.send(result);
+});
+
 //ルーティング
 app.get('/',(req,res)=>{
     res.send('Hello Express!');
